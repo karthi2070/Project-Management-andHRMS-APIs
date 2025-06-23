647b783bfe1cf4ab5a88fb871825cae6dcc45bb6
bazee <bazee@aws.com>
2025-06-23 09:33:18 +0530

    epm module changes

--- src/models/empolyeeModel.js
+++ src/models/empolyeeModel.js
@@ -34,7 +34,7 @@ const pool = require('../config/db');
 //id, name, employee_id, phone, mail, dob, doj, department, designation, salary, status, status_reson, status_desc, pan, aadhar, education, address, city, state, pincode, p_address, p_city, p_state, p_pincode, is_deleted, created_at, updated_at
      async getAllEmployees() {
          try {
-             const sql = `SELECT user_id,name FROM employee_tbl WHERE is_deleted = 0`;
+             const sql = `SELECT * FROM employee_tbl WHERE is_deleted = 0`;
              const [employees] = await pool.query(sql);
              return employees;
          } catch (error) {
