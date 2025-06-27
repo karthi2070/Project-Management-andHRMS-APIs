fd0d5311b7b62a75a975729e7ae297b95a56563d
karthi2070 <karthikarthi2070@gmail.com>
2025-06-27 12:46:24 +0530

    update to pm node fetch

--- src/config/openAi.js
+++ src/config/openAi.js
@@ -1,8 +1,10 @@
+const fetch = require('node-fetch');
 const OpenAI = require('openai');
 require('dotenv').config();
 
 const openai = new OpenAI({
  apiKey: process.env.OPENAI_API_KEY,
+ fetch: fetch
 });
 
 module.exports = openai;
