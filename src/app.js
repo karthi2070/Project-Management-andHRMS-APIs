b20079bc0944aea691b95b9bcd5675ef4fad7ea6
karthi2070 <karthikarthi2070@gmail.com>
2025-07-05 10:50:17 +0530

    check origins

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: function (origin, callback) {
-
+console.log('CORS Origin:', origin);
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
